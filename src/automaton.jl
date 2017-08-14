de06320b51862f14a882bf3712986882baefd8a6
Fredrik Hagebring <fredrik@hagebring.com>
2017-08-14 14:15:55 +0200

    Fixed a bug with the construction of Automatons when using UnitRange

--- src/automaton.jl
+++ src/automaton.jl
@@ -24,31 +24,38 @@ type Automaton
             marked = IntSet(),
             uncontrollable = IntSet()
         )
+        states = IntSet(collect(states)),
+        events = IntSet(collect(events)),
+        transitions = Set{Transition}(transitions),
+        init = IntSet(collect(init)),
+        marked = IntSet(collect(marked)),
+        uncontrollable = IntSet(collect(uncontrollable))
+
 
         # Verify transitions
         for (source,event,target) in transitions
-            source in states || push!(collect(states), source)
-            event in events || push!(collect(events), event)
-            target in states || push!(collect(states), target)
+            source in states || push!(states, source)
+            event in events || push!(events, event)
+            target in states || push!(states, target)
         end
 
         # Verify init states
         for q in init
-            (q in states) || push!(collect(states), q)
+            (q in states) || push!(states, q)
         end
 
         # Verify marked states
         for q in marked
-            (q in states) || push!(collect(states), q)
+            (q in states) || push!(states, q)
         end
 
         # Verify uncontrollable events
         for e in uncontrollable
-            (e in events) || push!(collect(events), e)
+            (e in events) || push!(events, e)
         end
 
         controllable = setdiff(events, uncontrollable)
-        new(IntSet(states), IntSet(events), Set{Transition}(transitions), IntSet(init), IntSet(marked), IntSet(controllable), IntSet(uncontrollable))
+        new(states, events, transitions, init, marked, controllable, uncontrollable)
     end
 end
 Automaton(ns::Int) = Automaton(states = IntSet(1:ns))
